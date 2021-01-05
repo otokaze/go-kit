4b34c292527086c0d67f9636f3fcd9f87dc69b3e
音風 <admin@otokaze.cn>
2021-01-05 16:58:00 +0800

    up

--- log/log.go
+++ log/log.go
@@ -16,16 +16,16 @@ var (
 
 func Error(format string, a ...interface{}) {
 	ts := time.Now().Local().Format("2006-01-02 15:04:05")
-	format = fmt.Sprintf("%s %s %s", errPrefix, ts, format)
+	format = fmt.Sprintf("%s %s %s\n", errPrefix, ts, format)
 	printcolor.Fred(os.Stderr, format, a...)
 }
 func Warn(format string, a ...interface{}) {
 	ts := time.Now().Local().Format("2006-01-02 15:04:05")
-	format = fmt.Sprintf("%s %s %s", warnPrefix, ts, format)
+	format = fmt.Sprintf("%s %s %s\n", warnPrefix, ts, format)
 	printcolor.Fyellow(os.Stdout, format, a...)
 }
 func Info(format string, a ...interface{}) {
 	ts := time.Now().Local().Format("2006-01-02 15:04:05")
-	format = fmt.Sprintf("%s %s %s", infoPrefix, ts, format)
+	format = fmt.Sprintf("%s %s %s\n", infoPrefix, ts, format)
 	fmt.Fprintf(os.Stdout, format, a...)
 }
