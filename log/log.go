d836b9bbc4e24aaef8d813d3e35cb9ed929a81ef
Otokaze <i@otokaze.me>
2021-10-31 23:32:22 +0800

    fix depath

--- log/log.go
+++ log/log.go
@@ -19,17 +19,17 @@ var (
 
 func Error(format string, a ...interface{}) {
 	ts := time.Now().Local().Format("2006-01-02 15:04:05")
-	format = fmt.Sprintf("%s %s %s %s\n", errPrefix, ts, caller(1), format)
+	format = fmt.Sprintf("%s %s %s %s\n", errPrefix, ts, caller(2), format)
 	printcolor.Fred(os.Stderr, format, a...)
 }
 func Warn(format string, a ...interface{}) {
 	ts := time.Now().Local().Format("2006-01-02 15:04:05")
-	format = fmt.Sprintf("%s %s %s %s\n", warnPrefix, ts, caller(1), format)
+	format = fmt.Sprintf("%s %s %s %s\n", warnPrefix, ts, caller(2), format)
 	printcolor.Fyellow(os.Stdout, format, a...)
 }
 func Info(format string, a ...interface{}) {
 	ts := time.Now().Local().Format("2006-01-02 15:04:05")
-	format = fmt.Sprintf("%s %s %s %s\n", infoPrefix, ts, caller(1), format)
+	format = fmt.Sprintf("%s %s %s %s\n", infoPrefix, ts, caller(2), format)
 	fmt.Fprintf(os.Stdout, format, a...)
 }
 
